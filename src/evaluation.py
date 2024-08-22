0cf61b2e71976ea2ff001d70328ae649acfa4a45
silkrow <si1krow@outlook.com>
2024-08-23 02:59:13 +0800

    some udpates

--- src/evaluation.py
+++ src/evaluation.py
@@ -11,6 +11,7 @@ class Evaluation:
         self.conf = conf
 
     def material_count(self, board):
+        # Define some constants
         piece_values = {
             chess.PAWN: 1,
             chess.KNIGHT: 3,
@@ -18,6 +19,14 @@ class Evaluation:
             chess.ROOK: 5,
             chess.QUEEN: 9
         }
+
+        center_squares = (chess.E4, chess.E5, chess.D4, chess.D5)
+        second_center = (chess.C3, chess.D3, chess.E3, chess.F3, chess.F4, chess.F5, chess.F6, chess.E6, chess.D6, chess.C6, chess.C5, chess.C4)
+        corner_squares = (chess.A1, chess.A8, chess.H1, chess.H8)
+        side_squares = (chess.A2, chess.A3, chess.A4, chess.A5, chess.A6, chess.A7, \
+                        chess.H2, chess.H3, chess.H4, chess.H5, chess.H6, chess.H7, \
+                        chess.B1, chess.C1, chess.D1, chess.E1, chess.F1, chess.G1, \
+                        chess.B8, chess.C8, chess.D8, chess.E8, chess.F8, chess.G8)
         
         white_material = 0
         black_material = 0
@@ -26,11 +35,22 @@ class Evaluation:
         for square in chess.SQUARES:
             piece = board.piece_at(square)
             if piece:
-                # Add piece value to the corresponding side's material count
+                piece_val = piece_values.get(piece.piece_type, 0)
+                if piece.piece_type is not chess.KING:
+                    if square in center_squares:
+                        piece_val += 0.04
+                    elif square in second_center:
+                        piece_val += 0.02
+                if piece.piece_type in (chess.KNIGHT, chess.BISHOP):
+                    if square in corner_squares:
+                        piece_val -= 0.05
+                    elif square in side_squares:
+                        piece_val -= 0.03
+
                 if piece.color == chess.WHITE:
-                    white_material += piece_values.get(piece.piece_type, 0)
+                    white_material += piece_val
                 else:
-                    black_material += piece_values.get(piece.piece_type, 0)
+                    black_material += piece_val
 
         return white_material, black_material
 
