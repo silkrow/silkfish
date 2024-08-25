c96b482a644bbe86b3f77b38f8532889bba99f56
silkrow <si1krow@outlook.com>
2024-08-25 09:28:52 +0800

    evaluation function modification

--- src/evaluation.py
+++ src/evaluation.py
@@ -21,12 +21,9 @@ class Evaluation:
         }
 
         center_squares = (chess.E4, chess.E5, chess.D4, chess.D5)
-        second_center = (chess.C3, chess.D3, chess.E3, chess.F3, chess.F4, chess.F5, chess.F6, chess.E6, chess.D6, chess.C6, chess.C5, chess.C4)
         corner_squares = (chess.A1, chess.A8, chess.H1, chess.H8)
         side_squares = (chess.A2, chess.A3, chess.A4, chess.A5, chess.A6, chess.A7, \
-                        chess.H2, chess.H3, chess.H4, chess.H5, chess.H6, chess.H7, \
-                        chess.B1, chess.C1, chess.D1, chess.E1, chess.F1, chess.G1, \
-                        chess.B8, chess.C8, chess.D8, chess.E8, chess.F8, chess.G8)
+                        chess.H2, chess.H3, chess.H4, chess.H5, chess.H6, chess.H7)
         
         white_material = 0
         black_material = 0
@@ -39,8 +36,6 @@ class Evaluation:
                 if piece.piece_type is not chess.KING:
                     if square in center_squares:
                         piece_val += 0.04
-                    # elif square in second_center:
-                    #     piece_val += 0.02
                 if piece.piece_type in (chess.KNIGHT, chess.BISHOP):
                     if square in corner_squares:
                         piece_val -= 0.05
