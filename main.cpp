95a09fe75413023be55143947484ffb78ea84676
silkrow <si1krow@outlook.com>
2024-09-05 05:17:01 -0500

    a roughly working version

--- main.cpp
+++ main.cpp
@@ -69,19 +69,44 @@ int minimax (Board& board, int depth, int alpha, int beta, Color color) {
 }
 
 int main (int argc, char *argv[]) {
-    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-
-    Movelist moves;
-    movegen::legalmoves(moves, board);
 	int depth = atoi(argv[1]);
+    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    Movelist moves;
+	Color turn = Color::WHITE;
 
-	auto start = std::chrono::high_resolution_clock::now();
-	int eval = minimax(board, depth, -MAX_SCORE, MAX_SCORE, Color::WHITE);	
-	auto end = std::chrono::high_resolution_clock::now();
-    chrono::duration<double> duration = end - start;
-    cout << "Execution time: " << duration.count() << " seconds\n";
+	string game_s = "";
 
-	printf("nodes: %ld\n", minimax_searched);
-	printf("eval: %d\n", eval);
+	do {
+		movegen::legalmoves(moves, board);
+		Move picked_move;
+		auto start = std::chrono::high_resolution_clock::now();
+		int eval = turn == Color::WHITE ? -MAX_SCORE:MAX_SCORE;
+		for (int i = 0; i < moves.size(); i++) {
+			const auto move = moves[i];
+			board.makeMove(move);
+			int move_eval = minimax(board, depth, -MAX_SCORE, MAX_SCORE, 1 - turn);	
+			board.unmakeMove(move);
+
+			if ((turn == Color::WHITE && move_eval > eval) || (turn == Color::BLACK && move_eval < eval)) {
+				eval = move_eval;
+				picked_move = move;
+			} 
+		}
+		auto end = std::chrono::high_resolution_clock::now();
+    	chrono::duration<double> duration = end - start;
+    	cout << "Execution time: " << duration.count() << " seconds\n";
+		printf("nodes: %ld\n", minimax_searched);
+		printf("eval: %d\n", eval);
+
+		if (!moves.empty()) {
+			string s = uci::moveToSan(board, picked_move);
+			game_s = game_s + " " + s;
+			cout << game_s << endl;
+			board.makeMove(picked_move);
+		}
+
+		turn = 1 - turn;
+
+	} while (!moves.empty());
     return 0;
 }
