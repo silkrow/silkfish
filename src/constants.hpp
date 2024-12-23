722d9fd2d26cb92b52e73e73801ee1d669eb7324
silkrow <si1krow@outlook.com>
2024-12-22 19:04:44 -0600

    changed q depth default to 4

--- src/constants.hpp
+++ src/constants.hpp
@@ -12,7 +12,7 @@ const int BOARD_SIZE = 64;
 const int CASTLE = 10;
 
 const int DEFAULT_DEPTH_MM = 7;
-const int DEFAULT_DEPTH_Q = 3;
+const int DEFAULT_DEPTH_Q = 4;
 
 const int PIECE_VAL[6] = {1, 3, 3, 5, 9, 0};
 const int RAND_MOVE_THRE = 10;
