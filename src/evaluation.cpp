7439631215be3a5454bf9fb1ae702e097a8a2a84
silkrow <si1krow@outlook.com>
2024-12-23 20:32:09 -0600

    debugging

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -51,161 +51,158 @@ int evaluation(chess::Board& board) {
 
     // Define endgame: 
     // Botj sides have less than 13 points of materials
-	// int white_points = 0, black_points = 0;
-    // for (int i = 0; i < BOARD_SIZE; i++) {
-	// 	auto piece = board.at(chess::Square((uint8_t)i));
-	// 	if (piece != chess::Piece::NONE) {
-	// 		if (piece.color() == chess::Color::WHITE) {
-	// 			white_points += PIECE_VAL[piece.type()];
-	// 		} else {
-	// 			black_points += PIECE_VAL[piece.type()];
-	// 		}
-	// 	} 
-	// }    
+	uint64_t mask = board.all().getBits();
+	int white_points = 0, black_points = 0;
+    for (int i = 0; i < BOARD_SIZE; i++) {
+		if (mask & 1ULL << i) {
+			auto piece = board.at(chess::Square((uint8_t)i));
+			if (piece.color() == chess::Color::WHITE) {
+				white_points += PIECE_VAL[piece.type()];
+			} else {
+				black_points += PIECE_VAL[piece.type()];
+			}
+		}
+	}    
 
-    // int offset = (white_points <= 13 && black_points <= 13)? 1:0; // 1 for eg, 0 for mg
-	// int offset = 0;
-
-	// std::cout << board.pieces(chess::PieceType::ROOK, chess::Color::WHITE) << std::endl;
-	// std::cout << board.us(chess::Color::BLACK) << std::endl;
-
-	// for (int i = 0; i < BOARD_SIZE; i++) {
-	// 	chess::PieceType piecetype = board.at<chess::PieceType>(chess::Square((uint8_t)i));
-	// 	if (piecetype != chess::PieceType::NONE) {
-	// 		if (board.at(chess::Square((uint8_t)i)).color() == chess::Color::WHITE) {
-	// 			evaluation += PESTO_POSITION[offset][piecetype][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[offset][piecetype];
-	// 		} else {
-	// 			evaluation -= (PESTO_POSITION[offset][piecetype][i] + PESTO_VALUE[offset][piecetype]);
-	// 		}
-	// 	} 
-	// }
+    int offset = (white_points <= 13 && black_points <= 13)? 1:0; // 1 for eg, 0 for mg
+	
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		if (mask & 1ULL << i) {
+			chess::Piece piece = board.at<chess::Piece>(chess::Square((uint8_t)i));
+			if (piece.color() == chess::Color::WHITE) {
+				evaluation += PESTO_POSITION[offset][piece.type()][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[offset][piece.type()];
+			} else {
+				evaluation -= (PESTO_POSITION[offset][piece.type()][i] + PESTO_VALUE[offset][piece.type()]);
+			}
+		}
+	}
 
 
-	int mg_white = 0, mg_black = 0, eg_white = 0, eg_black = 0;
-	uint64_t mask;
-	// PAWN
-	mask = board.pieces(chess::PieceType::PAWN, chess::Color::WHITE).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_white += PESTO_POSITION[0][0][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][0];
-			eg_white += PESTO_POSITION[1][0][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][0];
-        }
-		mask = mask >> 1;
-    }
-	// KNIGHT
-	mask = board.pieces(chess::PieceType::KNIGHT, chess::Color::WHITE).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_white += PESTO_POSITION[0][1][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][1];
-			eg_white += PESTO_POSITION[1][1][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][1];
-        }
-		mask = mask >> 1;
-    }
-	// BISHOP
-	mask = board.pieces(chess::PieceType::BISHOP, chess::Color::WHITE).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_white += PESTO_POSITION[0][2][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][2];
-			eg_white += PESTO_POSITION[1][2][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][2];
-        }
-		mask = mask >> 1;
-    }
-	// ROOK
-	mask = board.pieces(chess::PieceType::ROOK, chess::Color::WHITE).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_white += PESTO_POSITION[0][3][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][3];
-			eg_white += PESTO_POSITION[1][3][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][3];
-        }
-		mask = mask >> 1;
-    }
-	// QUEEN
-	mask = board.pieces(chess::PieceType::QUEEN, chess::Color::WHITE).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_white += PESTO_POSITION[0][4][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][4];
-			eg_white += PESTO_POSITION[1][4][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][4];
-        }
-		mask = mask >> 1;
-    }
-	// KING
-	mask = board.pieces(chess::PieceType::KING, chess::Color::WHITE).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_white += PESTO_POSITION[0][5][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][5];
-			eg_white += PESTO_POSITION[1][5][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][5];
-        }
-		mask = mask >> 1;
-    }
+	// int mg_white = 0, mg_black = 0, eg_white = 0, eg_black = 0;
+	// uint64_t mask;
+	// // PAWN
+	// mask = board.pieces(chess::PieceType::PAWN, chess::Color::WHITE).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_white += PESTO_POSITION[0][0][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][0];
+	// 		eg_white += PESTO_POSITION[1][0][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][0];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // KNIGHT
+	// mask = board.pieces(chess::PieceType::KNIGHT, chess::Color::WHITE).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_white += PESTO_POSITION[0][1][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][1];
+	// 		eg_white += PESTO_POSITION[1][1][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][1];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // BISHOP
+	// mask = board.pieces(chess::PieceType::BISHOP, chess::Color::WHITE).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_white += PESTO_POSITION[0][2][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][2];
+	// 		eg_white += PESTO_POSITION[1][2][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][2];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // ROOK
+	// mask = board.pieces(chess::PieceType::ROOK, chess::Color::WHITE).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_white += PESTO_POSITION[0][3][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][3];
+	// 		eg_white += PESTO_POSITION[1][3][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][3];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // QUEEN
+	// mask = board.pieces(chess::PieceType::QUEEN, chess::Color::WHITE).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_white += PESTO_POSITION[0][4][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][4];
+	// 		eg_white += PESTO_POSITION[1][4][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][4];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // KING
+	// mask = board.pieces(chess::PieceType::KING, chess::Color::WHITE).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_white += PESTO_POSITION[0][5][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[0][5];
+	// 		eg_white += PESTO_POSITION[1][5][BOARD_SIZE - 1 - (8*(i/8) + 7 - i%8)] + PESTO_VALUE[1][5];
+    //     }
+	// 	mask = mask >> 1;
+    // }
 
-	// PAWN
-	mask = board.pieces(chess::PieceType::PAWN, chess::Color::BLACK).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_black += PESTO_POSITION[0][0][i] + PESTO_VALUE[0][0];
-			eg_black += PESTO_POSITION[1][0][i] + PESTO_VALUE[1][0];
-        }
-		mask = mask >> 1;
-    }
-	// KNIGHT
-	mask = board.pieces(chess::PieceType::KNIGHT, chess::Color::BLACK).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_black += PESTO_POSITION[0][1][i] + PESTO_VALUE[0][1];
-			eg_black += PESTO_POSITION[1][1][i] + PESTO_VALUE[1][1];
-        }
-		mask = mask >> 1;
-    }
-	// BISHOP
-	mask = board.pieces(chess::PieceType::BISHOP, chess::Color::BLACK).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_black += PESTO_POSITION[0][2][i] + PESTO_VALUE[0][2];
-			eg_black += PESTO_POSITION[1][2][i] + PESTO_VALUE[1][2];
-        }
-		mask = mask >> 1;
-    }
-	// ROOK
-	mask = board.pieces(chess::PieceType::ROOK, chess::Color::BLACK).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_black += PESTO_POSITION[0][3][i] + PESTO_VALUE[0][3];
-			eg_black += PESTO_POSITION[1][3][i] + PESTO_VALUE[1][3];
-        }
-		mask = mask >> 1;
-    }
-	// QUEEN
-	mask = board.pieces(chess::PieceType::QUEEN, chess::Color::BLACK).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_black += PESTO_POSITION[0][4][i] + PESTO_VALUE[0][4];
-			eg_black += PESTO_POSITION[1][4][i] + PESTO_VALUE[1][4];
-        }
-		mask = mask >> 1;
-    }
-	// KING
-	mask = board.pieces(chess::PieceType::KING, chess::Color::BLACK).getBits();
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		if (mask == 0) break;
-        if (mask & 1ULL) { 
-            mg_black += PESTO_POSITION[0][5][i] + PESTO_VALUE[0][5];
-			eg_black += PESTO_POSITION[1][5][i] + PESTO_VALUE[1][5];
-        }
-		mask = mask >> 1;
-    }
+	// // PAWN
+	// mask = board.pieces(chess::PieceType::PAWN, chess::Color::BLACK).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_black += PESTO_POSITION[0][0][i] + PESTO_VALUE[0][0];
+	// 		eg_black += PESTO_POSITION[1][0][i] + PESTO_VALUE[1][0];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // KNIGHT
+	// mask = board.pieces(chess::PieceType::KNIGHT, chess::Color::BLACK).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_black += PESTO_POSITION[0][1][i] + PESTO_VALUE[0][1];
+	// 		eg_black += PESTO_POSITION[1][1][i] + PESTO_VALUE[1][1];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // BISHOP
+	// mask = board.pieces(chess::PieceType::BISHOP, chess::Color::BLACK).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_black += PESTO_POSITION[0][2][i] + PESTO_VALUE[0][2];
+	// 		eg_black += PESTO_POSITION[1][2][i] + PESTO_VALUE[1][2];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // ROOK
+	// mask = board.pieces(chess::PieceType::ROOK, chess::Color::BLACK).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_black += PESTO_POSITION[0][3][i] + PESTO_VALUE[0][3];
+	// 		eg_black += PESTO_POSITION[1][3][i] + PESTO_VALUE[1][3];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // QUEEN
+	// mask = board.pieces(chess::PieceType::QUEEN, chess::Color::BLACK).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_black += PESTO_POSITION[0][4][i] + PESTO_VALUE[0][4];
+	// 		eg_black += PESTO_POSITION[1][4][i] + PESTO_VALUE[1][4];
+    //     }
+	// 	mask = mask >> 1;
+    // }
+	// // KING
+	// mask = board.pieces(chess::PieceType::KING, chess::Color::BLACK).getBits();
+	// for (int i = 0; i < BOARD_SIZE; ++i) {
+	// 	if (mask == 0) break;
+    //     if (mask & 1ULL) { 
+    //         mg_black += PESTO_POSITION[0][5][i] + PESTO_VALUE[0][5];
+	// 		eg_black += PESTO_POSITION[1][5][i] + PESTO_VALUE[1][5];
+    //     }
+	// 	mask = mask >> 1;
+    // }
 
-	evaluation = mg_white - mg_black;
+	// evaluation = mg_white - mg_black;
 	return evaluation;
 }
 
